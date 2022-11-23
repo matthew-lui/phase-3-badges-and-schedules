291d257cfe0b2d8de05ab732885b80154d99030b
matthew-lui <107003143+matthew-lui@users.noreply.github.com>
2022-11-23 12:04:34 -0500

    commit tree 62993a35ee2873b4b4596fc53a535b1cd70fca43

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
-# Write your code here.
+def badge_maker(name)
+     "Hello, my name is #{name}."
+end
+
+test_result = badge_maker("Arel")
+puts test_result
+
+def batch_badge_creator(attendees)
+    attendees.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(attendees)
+    @room = 0
+    attendees.map do |attendee|
+        @room += 1
+        "Hello, #{attendee}! You'll be assigned to room #{@room}!"
+    end
+end
+
+def printer(attendees)
+    room_assignments && batch_badge_creator.each do |antendee|
+        puts attendee
+end
+
+
+
+
+
+
+
+
+
+
+
